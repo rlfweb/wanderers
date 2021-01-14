6cb5ce3065df7edaebd8fe37a7f3b7de3df34fcf
Rebecca French <rlfweb8@gmail.com>
2021-01-14 14:21:00 +0000

    Adding a photos fade and movement

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,26 @@ barba.init({
   transitions: [
     {
       name: "next",
+
       leave({ current, next, trigger }) {
         return new Promise((resolve) => {
           const timeline = gsap.timeline({
             onComplete() {
+              current.container.remove();
               resolve();
             },
           });
           const navigation = current.container.querySelectorAll(
             "header, a.next, a.previous"
           );
-          timeline.to(navigation, { opacity: 0 });
+          const photos = current.container.querySelectorAll("div.photos");
+
+          timeline
+            .to(navigation, { opacity: 0 }, 0)
+            .to(photos, { opacity: 0.25, x: 500 }, 0);
         });
       },
+
       enter({ current, next, trigger }) {
         return new Promise((resolve) => {
           const timeline = gsap.timeline({
@@ -26,9 +33,13 @@ barba.init({
           const navigation = next.container.querySelectorAll(
             "header, a.next, a.previous"
           );
+          const photos = next.container.querySelectorAll("div.photos");
+
           timeline
             .set(navigation, { opacity: 0 })
-            .to(navigation, { opacity: 1 });
+            .set(photos, { opacity: 0.25, x: -500 })
+            .to(navigation, { opacity: 1 }, 0)
+            .to(photos, { opacity: 1, x: 0 }, 0);
         });
       },
     },
