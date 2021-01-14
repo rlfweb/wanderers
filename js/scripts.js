4e1f9df8f7a1aa250599ed4b238a15f481d51c6e
Rebecca French <rlfweb8@gmail.com>
2021-01-14 14:59:30 +0000

    Adding an image to our interstitial

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 const bodyTag = document.querySelector("body");
 const wiper = document.createElement("div");
 wiper.classList.add("wiper");
+
+const wiperImage = document.createElement("img");
+wiperImage.setAttribute("src", "/images/logo.svg");
+
+wiper.appendChild(wiperImage);
+
 bodyTag.appendChild(wiper);
 
 barba.init({
@@ -24,9 +30,24 @@ barba.init({
 
           timeline
             .set(wiper, { x: "-100%" })
+            .set(wiperImage, { opacity: 0 })
             .to(navigation, { opacity: 0 }, 0)
             .to(photos, { opacity: 0.25, x: 500 }, 0)
             .to(wiper, { x: 0 }, 0);
+        });
+      },
+
+      beforeEnter({ current, next, trigger }) {
+        return new Promise((resolve) => {
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            },
+          });
+
+          timeline
+            .to(wiperImage, { opacity: 1 })
+            .to(wiperImage, { opacity: 0, delay: 1 });
         });
       },
 
